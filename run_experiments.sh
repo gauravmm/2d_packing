a2fdddf90eff6b8a6e04ad5fa9fa26c947a58bca
Gaurav Manek <gaurav@gauravmanek.com>
2022-01-04 23:05:28 +0800

    Updated run_experiments script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for FILEN in 10 09 08 07 06 05 03
+for FILEN in 01 02 04 10 09 08 07 06 05 03
 do
     for PROB in {1..10}
     do
-        echo julia simple_packing.jl "data/unibo/Class_${FILEN}.2bp" "$PROB" | tee "class$FILEN-prob$PROB.txt"
+        echo julia simple_packing.jl "data/unibo/Class_${FILEN}.2bp" "$PROB" | tee "preproc-class$FILEN-prob$PROB.txt"
     done
 done
